a62146b303592a9b56ea1d4a110f2407cba4415a
alexyang4358 <alexyang4358@gmail.com>
2023-06-12 17:18:53 -0400

    categories display

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Category from './Category'
+import Quiz from './Quiz'
+
+function App() {
+
+  return (
+    <>
+      <Category />
+      <Quiz />
+    </>
+  )
+}
+
+export default App
